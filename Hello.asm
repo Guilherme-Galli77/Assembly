5d8987eb493f53c71cfa9c7805d37d632514ac7d
Guilherme-Galli77 <79452612+Guilherme-Galli77@users.noreply.github.com>
2021-05-05 22:40:14 -0300

    Create Hello.asm

---
+++ Hello.asm
@@ -0,0 +1,24 @@
+;----------------------------------
+; Programa Hello World
+;----------------------------------
+
+;Declaração das variaveis
+section .data:
+	msg db 'Hello World', 0xa
+	len equ $ - msg
+
+
+section .text:
+
+global _start
+
+_start:
+	mov edx, len
+	mov ecx, msg
+	mov ebx, 1
+	mov eax, 4
+	int 0x80
+; Saida
+	mov eax, 1
+	mov ebx, 0
+	int 0x80
